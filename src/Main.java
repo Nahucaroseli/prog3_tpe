e984ec516d134b012329c8ecae7bce2dde8237c7
Nahu Caroseli <nahucaroseli2100@gmail.com>
2024-05-04 14:19:35 -0300

    commit inicial

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String args[]) {
+        Servicios servicios = new Servicios("src/datasets/Procesadores.csv", "src/datasets/Tareas.csv");
+
+    }
+}
---
+++ src/Tarea.java
@@ -0,0 +1,42 @@
+
+public class Tarea {
+
+    private String ID;
+    private String nombre_tarea;
+    private Integer tiempo_ejecucion;
+    private boolean es_critica;
+    private Integer nivel_prioridad;
+
+
+    public Tarea(String nombre_tarea, String ID, Integer tiempo_ejecucion, boolean es_critica, Integer nivel_prioridad) {
+        this.nombre_tarea = nombre_tarea;
+        this.ID = ID;
+        this.tiempo_ejecucion = tiempo_ejecucion;
+        this.es_critica = es_critica;
+        this.nivel_prioridad = nivel_prioridad;
+    }
+
+
+    public String getNombre_tarea() {
+        return nombre_tarea;
+    }
+
+    public Integer getTiempo_ejecucion() {
+        return tiempo_ejecucion;
+    }
+
+    public boolean isEs_critica() {
+        return es_critica;
+    }
+
+    public Integer getNivel_prioridad() {
+        return nivel_prioridad;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+
+
+}
